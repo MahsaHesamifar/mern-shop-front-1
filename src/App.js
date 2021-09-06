537be21a6ec537bea8b66fcb8a587621549a55d1
FatemeSfariSarvandi <safari.sv.fa@gmail.com>
2021-09-06 21:37:45 +0430

    make productview component and footer component and fix bug of footer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Footer from './Components/Footer';
+import ProductView from './Components/ProductView';
+import './style/app.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App-container">
+            <ProductView />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Components/ProductView/ProductPrice/index.jsx
@@ -0,0 +1,19 @@
+const ProductPrice = () => {
+    return (
+        <div className="ProductPrice-container">
+            <div className="productPrice-set-position">
+                <div className="productPrice">
+                    <div className="add-to-shoppingCard">
+                        <div>Price info</div>
+                        <button>Add to shopping card</button>
+                    </div>
+                    <div className="other-price-info">
+                        <div></div>
+                        <div></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+export default ProductPrice;
