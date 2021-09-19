2fb20b86185ee0344d7cd526e9ab1decf046a4bc
MahsaHesamifar <m.hesamifar@yahoo.com>
2021-09-20 01:35:38 +0330

    adding logo image

---
+++ src/Components/Nav/LogoBtn/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import AddLogoBtn from "./AddLogoBtn/index";
+
+const LogoBtn = () => {
+  return (
+    <>
+      <div className="logo-photo">
+        <AddLogoBtn />
+      </div>
+    </>
+  );
+};
+export default LogoBtn;
--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,47 +1,43 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Logo from './LogoBtn';
+import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "./LogoBtn/index";
 
 const Nav = () => {
-    return (
-        <nav className="Nav-bar">
-            <ul className="ul">
-                <li className="li li-logo">
-                    <div className="logo-btn">
-                        <Logo />
-                    </div>
-                </li>
-                <li className="li li-search">
-                    <form className="input-container">
-                        <input
-                            className="search-input"
-                            type="text"
-                            placeholder=" Search"
-                        />
-                    </form>
-                </li>
-                <li className="li li-item home-page">
-                    <Link to="/" className="a-item">
-                        Home
-                    </Link>
-                </li>
-                <li className="li li-item">
-                    <Link to="/Categories" className="a-item">
-                        categories
-                    </Link>
-                </li>
-                <li className="li li-item">
-                    <Link to="/Basket" className="a-item">
-                        <i className="fas fa-shopping-cart"></i>
-                    </Link>
-                </li>
-                <li className="li li-profile">
-                    <Link to="/Profile">
-                        <div className="profile-btn">P</div>
-                    </Link>
-                </li>
-            </ul>
-        </nav>
-    );
+  return (
+    <nav className="Nav-bar">
+      <ul className="ul">
+        <li className="li li-logo">
+          <div className="logo-btn">
+            <Logo />
+          </div>
+        </li>
+        <li className="li li-search">
+          <form className="input-container">
+            <input className="search-input" type="text" placeholder=" Search" />
+          </form>
+        </li>
+        <li className="li li-item home-page">
+          <Link to="/" className="a-item">
+            Home
+          </Link>
+        </li>
+        <li className="li li-item">
+          <Link to="/Categories" className="a-item">
+            categories
+          </Link>
+        </li>
+        <li className="li li-item">
+          <Link to="/Basket" className="a-item">
+            <i className="fas fa-shopping-cart"></i>
+          </Link>
+        </li>
+        <li className="li li-profile">
+          <Link to="/Profile">
+            <div className="profile-btn">P</div>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 export default Nav;
