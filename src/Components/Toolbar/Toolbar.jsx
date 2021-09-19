ca0ed9c6085fdb068eab08370f3f93c5ff43f0d5
MahsaHesamifar <m.hesamifar@yahoo.com>
2021-09-19 19:07:46 +0330

    adding palettes to themeControl

--- src/Components/Toolbar/Toolbar.jsx
+++ src/Components/Toolbar/Toolbar.jsx
@@ -1,34 +1,43 @@
-import React from 'react';
-import Popup from '../Popup/Popup';
+import React, { useState } from "react";
+import Popup from "../Popup/Popup";
 
-const Toolbar = (props) => {
-    // const [popupName, setpopupName] = useState("+");
-    return (
-        <div className="toolbar-container">
-            <div className="toolbar-item edit-btn">
-                <span className="edit-label">edit: </span>
-                <div className="toggle">
-                    <input type="checkbox" />
-                </div>
-                {/* <button className="toolbar-btn edit-btn">O</button> */}
-            </div>
-            <div className="toolbar-item  theme-btn">
-                {/* <Popup /> */}
-                <Popup
-                    popup="theme"
-                    theme={props.theme}
-                    themeHandler={props.themeHandler}
-                />
-            </div>
+const Toolbar = props => {
+  const [isEditOn, setIsEditOn] = useState(true);
 
-            <div className="toolbar-item addProduct-btn">
-                <Popup popup="addProduct" />
-            </div>
-            {/* <div className="toolbar-item addLogo-btn">
+  // const [popupName, setpopupName] = useState("+");
+  return (
+    <div className="toolbar-container">
+      <div className="toolbar-item edit-btn">
+        <span className="edit-label">edit: </span>
+        <div className="toggle">
+          <input
+            type="checkbox"
+            checked={isEditOn}
+            onChange={() => {
+              setIsEditOn(!isEditOn);
+            }}
+          />
+          {/* {console.log(isEditOn)} */}
+        </div>
+        {/* <button className="toolbar-btn edit-btn">O</button> */}
+      </div>
+      <div className="toolbar-item  theme-btn">
+        {/* <Popup /> */}
+        <Popup
+          popup="theme"
+          theme={props.theme}
+          themeHandler={props.themeHandler}
+        />
+      </div>
+
+      <div className="toolbar-item addProduct-btn">
+        <Popup popup="addProduct" />
+      </div>
+      {/* <div className="toolbar-item addLogo-btn">
         <Popup popup="addLogo" />
       </div> */}
-        </div>
-    );
+    </div>
+  );
 };
 
 export default Toolbar;
